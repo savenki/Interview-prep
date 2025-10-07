a5a404f4025cdc9a6e7fd290b4828be8b274b1b5
savenki <atmakuru-sai.venkatesh@capgemini.com>
2025-10-07 16:44:21 +0530

    test build changes

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
+    browsers: ['ChromeHeadlessCI'],
+    singleRun: true,
+    restartOnFileChange: true
+  });
+};
