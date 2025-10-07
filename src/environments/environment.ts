d22291fa86e3565697c2d76190322c249109c757
savenki <atmakuru-sai.venkatesh@capgemini.com>
2025-10-07 18:41:34 +0530

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBqWc4q-5T4RmDMziAKoHB7kZqegb6bUrI',
+    authDomain: 'interview-prep-af37b.firebaseapp.com',
+    projectId: 'interview-prep-af37b',
+    storageBucket: 'interview-prep-af37b.firebasestorage.app',
+    messagingSenderId: '1036031448809',
+    appId: '1:1036031448809:web:4df214dd75598bb9cd8278',
+    measurementId: 'G-3WBTJ702WQ',
+  },
+};
