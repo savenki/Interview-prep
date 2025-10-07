a58ed9d9536c71c22c5d1b575761d6b8df477b62
savenki <atmakuru-sai.venkatesh@capgemini.com>
2025-10-07 16:55:56 +0530

    webpack added karma chrome launcher

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     customLaunchers: {
       ChromeHeadlessCI: {
